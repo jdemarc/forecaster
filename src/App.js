6414123b10bd6c538f06fd5bef68fa91f63bed5f
joseph <josephjohndemarco@gmail.com>
2020-10-04 22:30:15 -0400

    Add API call. Connect API to app.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
+import Day from './components/Day';
 import WeatherPanel from './components/WeatherPanel';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="Weather-hdr">
-          5-Day Forecast
-        </header>
-  
-        <div>
-          <WeatherPanel />
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      weather: []
+    }
+  }
+
+  componentDidMount() {
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+    const URL = `http://api.openweathermap.org/data/2.5/forecast?id=4487042&cnt=5&appid=${API_KEY}`
+    fetch(URL)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            weather: result.list
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+    render() {
+      const { error, isLoaded, weather } = this.state
+      if (error) {
+        return <div>Error: {error.message}</div>
+      } else if (!isLoaded) {
+        return <div>Loading...</div>
+      } else {
+      return (
+        <div className="App">
+          <header className="Weather-hdr">
+            5-Day Forecast
+          </header>
+    
+          <div>
+            <ul>
+              {weather.map((day) => (
+                <li key={day.dt}> {day.dt} </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
