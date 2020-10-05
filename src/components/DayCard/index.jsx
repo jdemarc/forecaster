2539e9e9e9c51648b16b7e08f5ab7fef6f1a8ed7
joseph <josephjohndemarco@gmail.com>
2020-10-05 15:45:39 -0400

    Add more details to day card and work to add weather icons.

--- src/components/DayCard/index.jsx
+++ src/components/DayCard/index.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import 'weather-icons/css/weather-icons.css';
+import styles from './DayCard.module.css'
 const moment = require('moment')
 
 function DayCard(props) {
     console.log(props)
+    const dayInfo = props.dayInfo
+    const imgURL = `wi wi-owm-day-${dayInfo.weather[0].id}`
+    console.log(dayInfo.weather[0].id)
+    console.log(imgURL)
 
-    const newDate = new Date();
-    const weekday = props.dayInfo.dt * 1000
+    const newDate = new Date()
+    const weekday = dayInfo.dt * 1000
     newDate.setTime(weekday)
 
     return (
-        <div>{moment(newDate).format('dddd')}</div>
+        <div className={styles.DayCard}>
+            <h4>{moment(newDate).format('dddd')}</h4>
+            <p>{moment(newDate).format('MMMM Do YYYY, h:mm a')}</p>
+            <i class={imgURL}></i>
+            <p>{dayInfo.weather[0].description}</p>
+            <p>{Math.floor(dayInfo.main.temp)}</p>
+        </div>
     )
 }
 
